9558ec0abf9d4c3f06f1258430068726b2bc83e4
ivfrost <vlpablo@tutanota.com>
2025-11-30 15:07:27 +0100

    Add ethernet detection to network module

--- waybar/scripts/network.sh
+++ waybar/scripts/network.sh
@@ -3,7 +3,12 @@
 SSID=$(nmcli -t -f ACTIVE,SSID dev wifi | grep yes | cut -d: -f2)
 SIGNAL=$(nmcli -t -f IN-USE,SIGNAL dev wifi | grep "^\*:" | cut -d: -f2)
 
-if [ -n "$SSID" ]; then
+ETHERNET=$(nmcli -t -f DEVICE,TYPE,STATE dev | grep "ethernet:connected" | cut -d: -f1)
+
+if [ -n "$ETHERNET" ]; then
+  ICON=""
+  echo "{\"text\":\"<span rise='-22pt' size='12pt'>$ICON</span>\",\"tooltip\":\"Ethernet: $ETHERNET (connected)\"}"
+elif [ -n "$SSID" ]; then
   if [ "$SIGNAL" -ge 70 ]; then
     ICON=""
   elif [ "$SIGNAL" -ge 40 ]; then
