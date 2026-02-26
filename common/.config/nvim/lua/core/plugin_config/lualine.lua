6bb2301031f0ab9890be7393422ba16b282c1771
ivfrost <ivfrost@github.com>
2026-02-26 22:44:30 +0100

    feat: start solarized theme variant

--- common/.config/nvim/lua/core/plugin_config/lualine.lua
+++ common/.config/nvim/lua/core/plugin_config/lualine.lua
@@ -1,44 +1,52 @@
--- One dark bubbles lualine theme
+-- Solarized Dark bubbles lualine theme
 local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
+  base03  = "#002b36",
+  base02  = "#073642",
+  base01  = "#586e75",
+  base00  = "#657b83",
+  base0   = "#839496",
+  base1   = "#93a1a1",
+  base2   = "#eee8d5",
+  base3   = "#fdf6e3",
+
+  yellow  = "#b58900",
+  orange  = "#cb4b16",
+  red     = "#dc322f",
+  magenta = "#d33682",
+  violet  = "#6c71c4",
+  blue    = "#268bd2",
+  cyan    = "#2aa198",
+  green   = "#859900",
 }
 
 local bubbles_theme = {
   normal = {
-    a = { fg = colors.black, bg = colors.blue },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.white },
+    a = { fg = colors.base03, bg = colors.blue },
+    b = { fg = colors.base1,  bg = colors.base02 },
+    c = { fg = colors.base1,  bg = colors.base03 },
   },
 
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
+  insert  = { a = { fg = colors.base03, bg = colors.cyan } },
+  visual  = { a = { fg = colors.base03, bg = colors.violet } },
+  replace = { a = { fg = colors.base03, bg = colors.red } },
 
   inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.white },
+    a = { fg = colors.base01, bg = colors.base02 },
+    b = { fg = colors.base01, bg = colors.base02 },
+    c = { fg = colors.base01, bg = colors.base03 },
   },
 }
 
 require('lualine').setup {
   options = {
     theme = bubbles_theme,
-   component_separators = '',
+    component_separators = '',
     section_separators = { left = '', right = '' },
   },
   sections = {
     lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
     lualine_b = { 'filename', 'branch' },
-    lualine_c = {
-      '%=', --[[ add your center components here in place of this comment ]]
-    },
+    lualine_c = { '%=' },
     lualine_x = {},
     lualine_y = { 'filetype', 'progress' },
     lualine_z = {
--- common/.config/nvim/lua/core/plugins.lua
+++ common/.config/nvim/lua/core/plugins.lua
@@ -22,7 +22,7 @@ return require('packer').startup(function(use)
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
   use 'nvim-tree/nvim-web-devicons'
-  use "navarasu/onedark.nvim"
+  use 'ishan9299/nvim-solarized-lua'
   if packer_bootstrap then
     require('packer').sync()
   end
