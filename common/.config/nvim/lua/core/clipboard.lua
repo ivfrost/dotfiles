2bba4591a9b87333f1ad7fdda390f6758bc3c819
ivfrost <ivfrost@github.com>
2026-06-01 18:56:25 +0200

    fix: nvim clipboard on X11

--- common/.config/nvim/lua/core/clipboard.lua
+++ common/.config/nvim/lua/core/clipboard.lua
@@ -1,13 +1,16 @@
--- Force Neovim to use wl-clipboard on Wayland
-vim.g.clipboard = {
-  name = "wl-clipboard",
-  copy = {
-    ["+"] = "wl-copy --foreground --type text/plain",
-    ["*"] = "wl-copy --foreground --type text/plain",
-  },
-  paste = {
-    ["+"] = "wl-paste --no-newline",
-    ["*"] = "wl-paste --no-newline",
-  },
-  cache_enabled = 0,
-}
+if os.getenv("WAYLAND_DISPLAY") then
+  vim.g.clipboard = {
+    name = "wl-clipboard",
+    copy = {
+      ["+"] = "wl-copy --foreground --type text/plain",
+      ["*"] = "wl-copy --foreground --type text/plain",
+    },
+    paste = {
+      ["+"] = "wl-paste --no-newline",
+      ["*"] = "wl-paste --no-newline",
+    },
+    cache_enabled = 0,
+  }
+else
+  vim.opt.clipboard = "unnamedplus"
+end
